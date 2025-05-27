37e9f865ee3e89b5f4a193aa955d2cd3cda7b42b
Ravibhushan1234 <rsravi70@gmail.com>
2025-05-27 11:54:49 +0530

    change in bhushan branch

--- src/main/java/practicegit/Practice.java
+++ src/main/java/practicegit/Practice.java
@@ -4,6 +4,8 @@ public class Practice {
 	public static void main() {
 		System.out.println("I am a simple person");
 		System.out.println("You are lucky");
+		//checkout
+		System.out.println("i ma innocent");
 	}
 
 }
